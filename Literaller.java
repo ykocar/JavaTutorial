b2453309557664747624712109c0b9d8f3cbc969
Yasin Kocar <77811733+ykocar@users.noreply.github.com>
2021-02-23 22:00:20 +0000

    Add files via upload

---
+++ Literaller.java
@@ -0,0 +1,12 @@
+package bolum02;
+
+public class Literaller {
+public static void main(String[] args) {
+	byte b = 127;
+	short s = 32000;
+	int i = 2147000000;
+	long t = 90000000000L;
+	float f = 1.0F;
+	double d = 1.0;
+}
+}
