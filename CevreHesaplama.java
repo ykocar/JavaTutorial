4dd5721ac38b6a08ba060a38c5cf0af372e9b791
Yasin Kocar <77811733+ykocar@users.noreply.github.com>
2021-02-28 23:33:33 +0000

    Add files via upload

---
+++ CevreHesaplama.java
@@ -0,0 +1,18 @@
+
+public class CevreHesaplama {
+
+public static void main(String[] args) {
+    double radius; // Declare radius
+    double round; // Declare area
+
+    // Assign a radius
+    radius = 5.5; // New value is radius
+
+    // Compute area
+    round = 2 * radius * 3.14159;
+
+    // Display results
+    System.out.println("The round for the circle of radius " +
+      radius + " is " + round);
+  }
+}
